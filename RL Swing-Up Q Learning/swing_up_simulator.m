6b171d4b458c2998ca5de43d3286bafad4780c8f
williamdolke <74454454+williamdolke@users.noreply.github.com>
2022-02-03 11:58:53 +0000

    Added epsilon-greedy exploration
    Epsilon can have an initial value and decay linearly as episodes progress to a minimum value

--- RL Swing-Up Q Learning/swing_up_simulator.m
+++ RL Swing-Up Q Learning/swing_up_simulator.m
@@ -22,7 +22,7 @@ Ts = Tc * 4;           % Time step for Reinforcement Learning agent
 rng('default');
 
 %% Critic
-buckets = 4; % Change in other script too
+buckets = 8; % Change in other script too
 
 Q_init = zeros(buckets,buckets,buckets,buckets,numAct);
 
@@ -40,7 +40,7 @@ end
 
 %% Learning
 doTraining = true;    % Set "true" for learning.
-maxEpisode = 17000;
+maxEpisode = 100000;
 
 if doTraining
     % Train the agent.
@@ -53,21 +53,19 @@ if doTraining
     results = sim('swing_up_sim','StartTime','0','StopTime','10');
     Q_init = results.Q;
     
-    episode = episode + 1;
     previousReward = results.reward;
     
-    while episode < maxEpisode
+    for episode=1:maxEpisode
         results = sim('swing_up_sim','StartTime','0','StopTime','10');
         Q_init = results.Q;
         plot([episode-1 episode], [previousReward results.reward], 'b-o')
-        episode = episode + 1;
         previousReward = results.reward;
     end
     save('Q_init.mat', 'Q_init')
-    save('Ts.mat', 'Ts')
 else
     % Load the pretrained agent.
     load('Q_init.mat', 'Q_init');
+    episode = 1;
 end
 
 %% Simulate and plot
